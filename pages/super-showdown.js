cdfe21be39324ed3a391eaf8dbcf3c6e08bb199a
Andrew Brice <bricea27@gmail.com>
2020-02-18 17:37:27 -0500

    added eslint and prettier

--- pages/super-showdown.js
+++ pages/super-showdown.js
@@ -1,80 +1,80 @@
-import getConfig from "next/config";
-import { useEffect, useState } from "react";
+import getConfig from 'next/config';
+import { useEffect, useState } from 'react';
 
-import { motion, Variants } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import Typography from "@material-ui/core/Typography";
+import Typography from '@material-ui/core/Typography';
 
 const { publicRuntimeConfig: config } = getConfig();
 const ID_LIMIT = 731;
 
 const getRandomId = () => {
-  return Math.floor(Math.random() * ID_LIMIT) + 1;
+	return Math.floor(Math.random() * ID_LIMIT) + 1;
 };
 
 export default function Index() {
-  const { API, SELF_URL } = config;
-  const [state, setState] = useState({ loading: true, characters: [] });
+	const { API, SELF_URL } = config;
+	const [state, setState] = useState({ loading: true, characters: [] });
 
-  const getCharacter = async id => {
-    const result = await fetch(`${SELF_URL}${API}/character/${id}`);
-    const data = await result.json();
-    return data;
-  };
+	const getCharacter = async id => {
+		const result = await fetch(`${SELF_URL}${API}/character/${id}`);
+		const data = await result.json();
+		return data;
+	};
 
-  const getCharacters = async () => {
-    const characters = await Promise.all([
-      getCharacter(getRandomId()),
-      getCharacter(getRandomId())
-    ]);
-    setState(state => ({ ...state, loading: false, characters }));
-  };
+	const getCharacters = async () => {
+		const characters = await Promise.all([
+			getCharacter(getRandomId()),
+			getCharacter(getRandomId())
+		]);
+		setState(state => ({ ...state, loading: false, characters }));
+	};
 
-  useEffect(() => {
-    getCharacters();
-  }, []);
+	useEffect(() => {
+		getCharacters();
+	}, []);
 
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.5
-      }
-    }
-  };
+	const container = {
+		hidden: { opacity: 0 },
+		show: {
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.5
+			}
+		}
+	};
 
-  const item = {
-    hidden: {
-      y: 50,
-      opacity: 0,
-      transition: {
-        y: { stiffness: 1000 }
-      }
-    },
-    show: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        y: { stiffness: 1000, velocity: -100 }
-      }
-    }
-  };
+	const item = {
+		hidden: {
+			y: 50,
+			opacity: 0,
+			transition: {
+				y: { stiffness: 1000 }
+			}
+		},
+		show: {
+			y: 0,
+			opacity: 1,
+			transition: {
+				y: { stiffness: 1000, velocity: -100 }
+			}
+		}
+	};
 
-  return (
-    <>
-      <Typography variant="h1">Super Showdown</Typography>
-      {!state.loading && (
-        <motion.div variants={container} initial="hidden" animate="show">
-          {state.characters.map(character => {
-            return (
-              <motion.div key={character.id} variants={item}>
-                <Typography variant="h2">{character.name}</Typography>
-              </motion.div>
-            );
-          })}
-        </motion.div>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Typography variant="h1">Super Showdown</Typography>
+			{!state.loading && (
+				<motion.div variants={container} initial="hidden" animate="show">
+					{state.characters.map(character => {
+						return (
+							<motion.div key={character.id} variants={item}>
+								<Typography variant="h2">{character.name}</Typography>
+							</motion.div>
+						);
+					})}
+				</motion.div>
+			)}
+		</>
+	);
 }
