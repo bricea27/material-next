e624c50af7573cbb084675bf8dbe9fb33f5ef30e
Andrew Brice <bricea27@gmail.com>
2020-02-18 17:26:15 -0500

    started playing with framer motion

--- pages/super-showdown.js
+++ pages/super-showdown.js
@@ -1,14 +1,20 @@
 import getConfig from "next/config";
 import { useEffect, useState } from "react";
 
+import { motion, Variants } from "framer-motion";
+
 import Typography from "@material-ui/core/Typography";
 
 const { publicRuntimeConfig: config } = getConfig();
 const ID_LIMIT = 731;
 
+const getRandomId = () => {
+  return Math.floor(Math.random() * ID_LIMIT) + 1;
+};
+
 export default function Index() {
   const { API, SELF_URL } = config;
-  const [state, setState] = useState({ characters: [] });
+  const [state, setState] = useState({ loading: true, characters: [] });
 
   const getCharacter = async id => {
     const result = await fetch(`${SELF_URL}${API}/character/${id}`);
@@ -16,32 +22,59 @@ export default function Index() {
     return data;
   };
 
-  const getRandomId = () => {
-    return Math.floor(Math.random() * ID_LIMIT) + 1;
-  };
-
   const getCharacters = async () => {
     const characters = await Promise.all([
       getCharacter(getRandomId()),
       getCharacter(getRandomId())
     ]);
-    setState(state => ({ ...state, characters }));
+    setState(state => ({ ...state, loading: false, characters }));
   };
 
   useEffect(() => {
     getCharacters();
   }, []);
 
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5
+      }
+    }
+  };
+
+  const item = {
+    hidden: {
+      y: 50,
+      opacity: 0,
+      transition: {
+        y: { stiffness: 1000 }
+      }
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: { stiffness: 1000, velocity: -100 }
+      }
+    }
+  };
+
   return (
     <>
       <Typography variant="h1">Super Showdown</Typography>
-      {state.characters.map(character => {
-        return (
-          <Typography key={character.id} variant="h2">
-            {character.name}
-          </Typography>
-        );
-      })}
+      {!state.loading && (
+        <motion.div variants={container} initial="hidden" animate="show">
+          {state.characters.map(character => {
+            return (
+              <motion.div key={character.id} variants={item}>
+                <Typography variant="h2">{character.name}</Typography>
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      )}
     </>
   );
 }
